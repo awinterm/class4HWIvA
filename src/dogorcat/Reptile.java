ef4be8811fdb6e205bf3ffefdc55b24d073603e8
andre_000 <andre_000@DrewsLaptop.PK5001Z>
2015-09-14 08:36:30 -0500

    First commit

---
+++ src/dogorcat/Reptile.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dogorcat;
+
+/**
+ *
+ * @author andre_000
+ */
+public abstract class Reptile implements Animal{
+            
+     private String name = "Jasper";
+     private String msg = "You Win I'm Jasper the Dog!";
+
+}
